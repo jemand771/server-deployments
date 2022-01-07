7f3522440785161a9784973f786b3eef4f847128
Willy <jemand771@gmx.net>
2022-01-07 20:56:56 +0100

    checkin of sealing script

---
+++ _repo-tools/seal-all.py
@@ -0,0 +1,16 @@
+import os
+import pathlib
+
+
+def main():
+    for ext in ("json", "yaml"):
+        for file in pathlib.Path("..").glob(f"**/*.{ext}s"):
+            new_file = file.parent / file.name[:-1]
+            if new_file.is_file():
+                continue
+            print(f"sealing {file} -> {new_file}")
+            os.system(f"D:/software/kubeseal/kubeseal -o {ext} <{file} >{new_file}")
+
+
+if __name__ == '__main__':
+    main()
